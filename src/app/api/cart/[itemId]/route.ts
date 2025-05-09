65177ff3bd1cb675f10d4a4be9ac7c6436ec1d65
n1sh1thaS <nishi2919@gmail.com>
2025-05-09 16:59:29 -0700

    fix lint error

--- src/app/api/cart/[itemId]/route.ts
+++ src/app/api/cart/[itemId]/route.ts
@@ -2,9 +2,10 @@ import clientPromise from "@/lib/db";
 import { ObjectId } from "mongodb";
 import { NextRequest, NextResponse } from "next/server";
 
-export async function DELETE(req: NextRequest, {params}: { params: { itemId: string } }){
+export async function DELETE(req: NextRequest, context: any){
     try {
-        const itemId = params.itemId;
+        const itemId = context.params.itemId;
+
         const client = await clientPromise;
         await client.db('store').collection('cart').deleteOne({_id: new ObjectId(itemId)})
         return NextResponse.json({status: 200})
